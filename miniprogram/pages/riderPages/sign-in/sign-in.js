a8c4098370332f1904ff651068b74b983f23ef99
FashionGod-Company's-Computer <1143162782@qq.com>
2021-02-04 14:26:44 +0800

    完善sign-in页面

--- miniprogram/pages/riderPages/sign-in/sign-in.js
+++ miniprogram/pages/riderPages/sign-in/sign-in.js
@@ -5,62 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    value: '',
+    phoneNumber: null,
+    password: null,
+    password_state_icon:'closed-eye',
+    password_state_type:'password'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 密码的显隐
+  onClickIcon() {
+    this.setData({
+      password_state_icon: this.data.password_state_icon=='closed-eye'? 'eye':'closed-eye',
+      password_state_type: this.data.password_state_type ? false:true,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  login() {
+    wx.navigateTo({
+      url: '../../riderPages/order/order',
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  register() {
+    wx.navigateTo({
+      url: '../../riderPages/sign-up/sign-up',
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
