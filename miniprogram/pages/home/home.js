d248752d1887653af137178c5e64f99070ea39d2
FashionGod-Company's-Computer <1143162782@qq.com>
2021-02-06 17:52:23 +0800

    非常good非常银杏的锚点联动

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -127,45 +127,9 @@ Page({
     }, 1000);
     console.log('淦，你碰到俺底部啦！');
   },
-
-/**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  navigateToDetail() {
+    wx.navigateTo({
+      url: '../userPages/merchant-delicious-detail/merchant-delicious-detail',
+    })
   }
 })
--- miniprogram/pages/userPages/merchant-delicious-detail/merchant-delicious-detail.js
+++ miniprogram/pages/userPages/merchant-delicious-detail/merchant-delicious-detail.js
@@ -6,7 +6,58 @@ Page({
    */
   data: {
     activeKey: 0,
+    // 标签锚点跳转值
+    indexMaodian: 'a',
+    // 标签详情内容锚点跳转
+    storeDetail: 'a1'
   },
+// 监听页面滑动距离
+onPageScroll(e) {
+  // 通过滑动的距离判断页面滑动那个区域让后让顶部的标签栏切换到对应位置
+  console.log('我在滑动')
+  var height = Number(e.detail.scrollTop)
+  console.log(height)
+  if (height <= 100) {
+   // 滑到1区域
+   this.setData({
+    indexMaodian: 'a',
+    activeKey: '1',
+   });
+  } else if (height > 100 && height<= 200) {
+   // 滑到2区域
+   this.setData({
+    indexMaodian: 'b',
+    activeKey: '2',
+   });
+  } else if (height > 200 && height <= 260) {
+  // 滑到3区域
+   this.setData({
+    indexMaodian: 'c',
+    activeKey: '3',
+   });
+  }
+ 
+ 
+ },
+ // 跳转到对应的标签详情内容区
+ toDetail(e) {
+   console.log(e)
+  let id = e.target.dataset.id
+  this.setData({
+   storeDetail: id
+  })
+ },
+ /**
+  * 生命周期函数--监听页面加载
+  */
+ onLoad: function (options) {
+  var systemInfo = wx.getSystemInfoSync();
+  var windowHeight = systemInfo.windowHeight;
+  // 拿到导航栏以下可视区域的高度
+  this.setData({
+   height: windowHeight
+  });
+ },
+ 
 
-  
 })
