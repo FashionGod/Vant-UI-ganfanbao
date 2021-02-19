089dd9a5be8585181faba139ec671da6f03ce926
unknown <unknown@example.com>
2021-02-19 23:31:39 +0800

    采用防抖函数，并彻底解决了动态锚点联动问题

--- miniprogram/pages/userPages/merchant-delicious-detail/merchant-delicious-detail.js
+++ miniprogram/pages/userPages/merchant-delicious-detail/merchant-delicious-detail.js
@@ -1,4 +1,5 @@
 // miniprogram/pages/userPages/merchant-delicious-detail/merchant-delicious-detail.js
+import tool from "../../public/tools/tool.js";
 Page({
 
   /**
@@ -164,7 +165,8 @@ Page({
           },
         ]
       },
-    ]
+    ],
+    arr: [0]
   },
   // 点击收藏
   collectionMerchant() {
@@ -172,34 +174,24 @@ Page({
       collectionStar: this.data.collectionStar == 'star-o' ? 'star' : 'star-o' 
     })
   },
-// 监听页面滑动距离
-onPageScroll(e) {
+// 滑动右边判断是哪个sidebar
+judgeScrollWhere(height) {
+  console.log('我动啦')
+  this.data.arr.forEach((item, i)=> {
+    if (height >= item && height < this.data.arr[i+1]) {
+       this.setData({
+        indexMaodian: `b${i+1}`,
+        activeKey: i,
+       });
+    }
+  })
+},
+onPageScroll: tool.debounce(function(res) {
   // 通过滑动的距离判断页面滑动那个区域让后让顶部的标签栏切换到对应位置
-  console.log(e)
-  var height = Number(e.detail.scrollTop)
-  console.log(height)
-  if (height <= 800) {
-   // 滑到1区域
-   this.setData({
-    indexMaodian: 'b1',
-    activeKey: '0',
-   });
-  } else if (height > 800 && height<= 1512) {
-   // 滑到2区域
-   this.setData({
-    indexMaodian: 'b2',
-    activeKey: '1',
-   });
-  } else if (height > 1512 && height <= 1821) {
-  // 滑到3区域
-   this.setData({
-    indexMaodian: 'b3',
-    activeKey: '2',
-   });
-  }
- 
- 
- },
+  console.log(res)
+  var height = Number(res[0].detail.scrollTop)
+  this.judgeScrollWhere(height)
+}, 100), 
  // 切换tab ‘点菜’ ‘评价’ ‘商家’
  switTab(event) {
    if (event.detail.name == 0) {
@@ -224,9 +216,11 @@ onPageScroll(e) {
  /**
   * 生命周期函数--监听页面加载
   */
- onLoad: function (options) {
-  var systemInfo = wx.getSystemInfoSync();
-  var windowHeight = systemInfo.windowHeight;
+ onLoad: function () {
+  this.data.merchantList.forEach((item, i)=>{
+    this.data.arr[i + 1] = 120*item.merchantDetailList.length + this.data.arr[i];
+  })
+  console.log(this.data.arr);
   // 加入锚点标记id
   var tmpCategoryList = this.data.categoryList
   var tmpMerchantList = this.data.merchantList
@@ -237,9 +231,7 @@ onPageScroll(e) {
   tmpMerchantList.forEach((item, i) => {
     item.id = 'a' + ( i + 1);
   })
-  // 拿到导航栏以下可视区域的高度
   this.setData({
-   height: windowHeight,
    categoryList: tmpCategoryList,
    merchantList: tmpMerchantList,
   });
