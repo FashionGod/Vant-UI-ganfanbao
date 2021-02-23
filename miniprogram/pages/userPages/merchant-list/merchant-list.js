d294c917a21b34d8a0861a381a131638279a3eda
FashionGod <1143162782@qq.com>
2021-02-23 23:23:17 +0800

    补充丢失的merchant-list页面

--- miniprogram/pages/userPages/merchant-list/merchant-list.js
+++ miniprogram/pages/userPages/merchant-list/merchant-list.js
@@ -1,4 +1,4 @@
-// miniprogram/pages/myPages/index/index.js
+// miniprogram/pages/userPages/merchant-list/merchant-list.js
 Page({
 
   /**
@@ -14,11 +14,6 @@ Page({
     pullDownloading: false,
     // 显示触底刷新
     scrollTouchedBottomLoading: false,
-    // 搜索框
-    value: '',
-    showSearchPanelFlag: false,
-    // 轮播图
-    swiperImgList: ['https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1901359579,1861271908&fm=26&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1840683049,2335736361&fm=26&gp=0.jpg' ,'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2905099515,729646340&fm=26&gp=0.jpg'],
     //商家列表筛选头部
     option1: [
       { text: '全部商品', value: 0 },
@@ -33,46 +28,8 @@ Page({
     ],
     value1: 0,
     value2: 0,
-    //
-  },
-  // 搜索框
-  onFocus() {
-    this.setData({
-      showSearchPanelFlag: true
-    })
-  },
-  onCancel() {
-    this.setData({
-      showSearchPanelFlag: false
-    })
-  },
-  onChange(e) {
-    this.setData({
-      value: e.detail,
-    });
-    console.log(e);  
-  },
-  onSearch() {
-    wx.showToast({
-      title: '搜索' + this.data.value,
-      icon: 'none'
-    })
   },
 
-  // 轮播图
-  previewImg(e) {
-    wx.previewImage({
-      current: this.data.swiperImgList[e.currentTarget.dataset.i], // 当前显示图片的http链接
-      urls: this.data.swiperImgList // 需要预览的图片http链接列表
-    })
-  },
-  // 子组件传来的，用来控制scroll的滚动，防止UI出现bug
-  handleScrollChange(e) {
-    const {dropDownForbidenScroll} = e.detail;
-    this.setData({
-      dropDownForbidenScroll: dropDownForbidenScroll,
-    });
-  },
   // 商家列表筛选头部
   onConfirm() {
     this.selectComponent('#item').toggle();
@@ -124,9 +81,4 @@ Page({
     }, 1000);
     console.log('淦，你碰到俺底部啦！');
   },
-  navigateToDetail() {
-    wx.navigateTo({
-      url: '../userPages/merchant-delicious-detail/merchant-delicious-detail',
-    })
-  }
 })
