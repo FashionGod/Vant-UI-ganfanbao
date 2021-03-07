065d375329d4bc0d1808cd6d6eaa6d7b1fa472b9
FashionGod <1143162782@qq.com>
2021-03-08 01:17:01 +0800

    商家订单卡片UI初步定型

--- miniprogram/components/merchant-order-card/merchant-order-card.js
+++ miniprogram/components/merchant-order-card/merchant-order-card.js
@@ -11,13 +11,17 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    activeNames: ['1'],
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onChange(event) {
+      this.setData({
+        activeNames: event.detail,
+      });
+    },
   }
 })
